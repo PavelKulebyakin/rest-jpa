c0ae2a44601a0826d73c5f7bbe37a8aff6f34425
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-13 21:15:08 +0300

    Add CommandLineRunner data loader

--- src/main/java/com/spring/practice/restjpa/RestJpaApplication.java
+++ src/main/java/com/spring/practice/restjpa/RestJpaApplication.java
@@ -1,7 +1,12 @@
 package com.spring.practice.restjpa;
 
+import com.spring.practice.restjpa.entity.Employee;
+import com.spring.practice.restjpa.service.EmployeeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class RestJpaApplication {
@@ -10,4 +15,13 @@ public class RestJpaApplication {
 		SpringApplication.run(RestJpaApplication.class, args);
 	}
 
+	@Bean
+	@Profile("test")
+	public CommandLineRunner dataLoader(EmployeeService service) {
+		return args -> {
+			service.saveEmployee(new Employee("Ivan", "Makarov", "IT", 100000));
+			service.saveEmployee(new Employee("Vladimir", "Boronin", "IT", 80000));
+			service.saveEmployee(new Employee("Anastasiya", "Volkova", "HR", 80000));
+		};
+	}
 }
