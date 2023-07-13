238fc54c2e8e6a6edb7c2be938f65b63997f514d
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-13 20:49:21 +0300

    Add Employee entity

---
+++ src/main/java/com/spring/practice/restjpa/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.spring.practice.restjpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    private String department;
+    private int salary;
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+}
