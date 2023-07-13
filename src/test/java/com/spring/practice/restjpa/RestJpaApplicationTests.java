3be9e997b117e1e493bedfdd9dc1f45856e1414d
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-07-13 20:41:40 +0300

    Create initial commit for REST application with Data JPA

---
+++ src/test/java/com/spring/practice/restjpa/RestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.practice.restjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
